ac267dffff892bede571b276da3f09a3bad63ea0
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2023-09-17 15:26:22 +0700

    Command line

---
+++ src/main/java/org/basic_cmd/Main.java
@@ -0,0 +1,8 @@
+package org.basic_cmd;
+
+public class Main {
+    public static void main(String[] args) {
+        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
+        dictionaryCommandline.dictionaryAdvanced();
+    }
+}
